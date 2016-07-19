88245a6aafe2d9644a3a8bee06da7679ad364c40
Dewey Hong <dhong@fortinet.com>
2016-07-19 16:47:31 -0700

    .

--- _support/vagrant/nserver/linode.sh
+++ _support/vagrant/nserver/linode.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
+sudo su
 set -x
 
-sudo echo "" > /root/.profile
-sudo echo "alias ll='ls -al'" >> /root/.profile
+echo "" > /root/.profile
+echo "alias ll='ls -al'" >> /root/.profile
 source /root/.profile
 
 export ip=$(ip addr show eth0 | grep -Po 'inet \K[\d.]+')
-sudo echo $ip 'dns.tz.com' >> /etc/hosts
+echo $ip 'dns.tz.com' >> /etc/hosts
 
-sudo ln -sf /usr/share/zoneinfo/EST /etc/localtime
+ln -sf /usr/share/zoneinfo/EST /etc/localtime
 
 exit 0
--- _support/vagrant/nserver/nserver.sh
+++ _support/vagrant/nserver/nserver.sh
@@ -1,37 +1,38 @@
 #!/usr/bin/env bash
 
+sudo su
 set -x
 
 # for dns server
 export ip=$(ip addr show eth0 | grep -Po 'inet \K[\d.]+')
 
-sudo echo '' >> /etc/resolv.conf
-sudo echo 'domain tz.com' >> /etc/resolv.conf
+echo '' >> /etc/resolv.conf
+echo 'domain tz.com' >> /etc/resolv.conf
 
-sudo apt-get update && apt-get upgrade -y
-sudo apt-get install bind9 bind9utils -y
-sudo apt-get install git -y
+apt-get update && apt-get upgrade -y
+apt-get install bind9 bind9utils -y
+apt-get install git -y
 
-sudo cp -Rf /vagrant/_support/vagrant/resource/named.conf.local /etc/bind
-sudo cp -Rf /vagrant/_support/vagrant/resource/db.tz.com /etc/bind
+cp -Rf /vagrant/_support/vagrant/resource/named.conf.local /etc/bind
+cp -Rf /vagrant/_support/vagrant/resource/db.tz.com /etc/bind
 
-sudo sed -i "s/PUBLIC_IP/$ip/g" /etc/bind/db.tz.com
+sed -i "s/PUBLIC_IP/$ip/g" /etc/bind/db.tz.com
 
-sudo service bind9 restart
+service bind9 restart
 
 dig @dns.tz.com tz.com
 
 # install webmin
-sudo apt-get install libnet-ssleay-perl libauthen-pam-perl libio-pty-perl apt-show-versions -y
-sudo wget http://prdownloads.sourceforge.net/webadmin/webmin_1.801_all.deb 
-sudo dpkg -i webmin_1.801_all.deb
+apt-get install libnet-ssleay-perl libauthen-pam-perl libio-pty-perl apt-show-versions -y
+wget http://prdownloads.sourceforge.net/webadmin/webmin_1.801_all.deb 
+dpkg -i webmin_1.801_all.deb
 echo "===================================="
 echo "access to " https://$ip:10000/
-sudo ufw allow 10000
-sudo service webmin restart
+ufw allow 10000
+service webmin restart
 
 # password for webmin : root / rootwebmin
-sudo echo -e "rootwebmin\nrootwebmin" | passwd
+echo -e "rootwebmin\nrootwebmin" | passwd
 
 exit 0
 
